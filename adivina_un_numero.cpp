7020de7259681feb17fd70dbd73e76c5d4a0268a
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-09-10 17:09:30 -0400

    Update adivina_un_numero.cpp

--- adivina_un_numero.cpp
+++ adivina_un_numero.cpp
@@ -9,22 +9,22 @@ int main() {
 
 	srand(time(NULL));
 
-	num = rand() % 100 + 1; // Vuelve la variable a un número random (del 1 al 100)
+	num = rand() % 100 + 1; // Vuelve la variable a un nÃºmero random (del 1 al 100)
 
-	std::cout << "---------- Adivina el Número ----------.\n";
+	std::cout << "---------- Adivina el NÃºmero ----------.\n";
 
 	do {
 
-		std::cout << "Ingrese un número entre 1 y 100:";
+		std::cout << "Ingrese un numero entre 1 y 100:";
 		std::cin >> adivinar;
 
 		intentos++;
 		
 		if (adivinar > num) {
-			std::cout << "Más bajo.\n";
+			std::cout << "Mas bajo.\n";
 		}
 		else if(adivinar < num) {
-			std::cout << "Más alto\n";
+			std::cout << "Mas alto\n";
 		}
 		else {
 			std::cout << "Correcto! intentos:" << intentos << "\n";
